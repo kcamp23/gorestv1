2fff7de9d3e5370660fd0c2b509fad94875e9f9e
Kat Camp <kcamp552369@gmail.com>
2022-03-18 13:48:23 -0400

    initial commit

---
+++ src/main/java/com/example/gorestv1/Controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.example.gorestv1.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RestController("/api/user")
+
+
+public class UserController {
+
+    
+}
