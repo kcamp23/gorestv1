7dc97bd5123bdd175cac1a879d85d949d0f9aafe
Kat Camp <kcamp552369@gmail.com>
2022-03-19 14:16:59 -0400

    initial commit

--- src/main/java/com/example/gorestv1/Controllers/UserController.java
+++ src/main/java/com/example/gorestv1/Controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.example.gorestv1.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RestController("/api/user")
@@ -8,5 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
-    
+    @Autowired
+    Enviroment env;
+    // /endpoint http://localhost:4444/api/user/token
+
+    @GetMapping("/token")
+    public String getToken () {
+        return env.getProperty("GOREST_TOKEN");
+    }
+
+    @GetMapping("/{id}")
+    public Object getOneUser (@PathVariable ("id") String userId, RestTemplate restTemplate){
+       String url = "https://gorest.co.in/public/v2/users/"+userId;
+               return  restTemplate.getForObject(url, Object.class);
+    }
 }
