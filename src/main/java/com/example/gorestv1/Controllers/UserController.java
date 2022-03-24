7d0f3ee212a568de03ba9f5f8fb0774271c79c3b
Kat Camp <kcamp552369@gmail.com>
2022-03-24 15:41:49 -0400

    controller setup

--- src/main/java/com/example/gorestv1/Controllers/UserController.java
+++ src/main/java/com/example/gorestv1/Controllers/UserController.java
@@ -1,11 +1,9 @@
 package com.example.gorestv1.Controllers;
 
+import com.example.gorestv1.models.UserModel;
 import com.fredhopper.environment.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -19,21 +17,44 @@ public class UserController {
     // /endpoint http://localhost:4444/api/user/token
 
     @GetMapping("/token")
-    public String getToken () {
-       // return env.getProperty("GOREST_TOKEN");
+    public String getToken() {
+        // return env.getProperty("GOREST_TOKEN");
         return env.getValue("GOREST_TOKEN");
     }
 
     @GetMapping("/{id}")
-    public Object getOneUser (@PathVariable ("id") String userId, RestTemplate restTemplate){
-       String url = "https://gorest.co.in/public/v2/users/"+userId;
-               return  restTemplate.getForObject(url, Object.class);
+    public Object getOneUser(@PathVariable("id") String userId, RestTemplate restTemplate) {
+        String url = "https://gorest.co.in/public/v2/users/" + userId;
+        return restTemplate.getForObject(url, Object.class);
     }
 
+    var user
+    UserModel =restTemplate.getForObject(url,UserModel .class);
+
+    assert user !=null;
+    System.out.println("Report: \n"+user.generateReport());
+
+    return user;
+    catch (Exception exception)
+
     {
+        System.out.println(exception.getClass());
+        return exception.getMessage();
+
+
         try {
             String url = "https://gorest.co.in/public/v2/users/" + userId;
             String apiToken = env.getProperty(GO_REST_TOKEN);
+            url += "?access-token=" + token
+
         }
-    }
+    @DeleteMapping("/{id}")
+public Object deleteOneUser(
+        @PathVariable ("id") String userId,
+        RestTemplate restTemplate
+
+            )}
+try {
+String url = ""
+}
 }
